bd792307db3111882e658d9dcd70538454d9e92c
Md Yousuf Ali <109690166+coderyousuf@users.noreply.github.com>
2025-11-28 11:08:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
-<?php
+// <?php
 
-// Return NaN if x or y is missing
-if (!isset($_GET['x']) || !isset($_GET['y'])) {
-    echo "NaN";
-    exit;
-}
+// // Return NaN if x or y is missing
+// if (!isset($_GET['x']) || !isset($_GET['y'])) {
+//     echo "NaN";
+//     exit;
+// }
 
-$x = $_GET['x'];
-$y = $_GET['y'];
+// $x = $_GET['x'];
+// $y = $_GET['y'];
 
-// Validate natural numbers (non-negative integers)
-if (!preg_match('/^\d+$/', $x) || !preg_match('/^\d+$/', $y)) {
-    echo "NaN";
-    exit;
-}
+// // Validate natural numbers (non-negative integers)
+// if (!preg_match('/^\d+$/', $x) || !preg_match('/^\d+$/', $y)) {
+//     echo "NaN";
+//     exit;
+// }
 
-// Use BCMath (handles unlimited big integers)
-function gcd($a, $b) {
-    while (bccomp($b, '0') !== 0) {
-        $tmp = $b;
-        $b = bcmod($a, $b);
-        $a = $tmp;
-    }
-    return $a;
-}
+// // Use BCMath (handles unlimited big integers)
+// function gcd($a, $b) {
+//     while (bccomp($b, '0') !== 0) {
+//         $tmp = $b;
+//         $b = bcmod($a, $b);
+//         $a = $tmp;
+//     }
+//     return $a;
+// }
 
-$x = (string)$x;
-$y = (string)$y;
+// $x = (string)$x;
+// $y = (string)$y;
 
-$g = gcd($x, $y);
+// $g = gcd($x, $y);
 
-// LCM = (x / gcd) * y
-$lcm = bcmul(bcdiv($x, $g), $y);
+// // LCM = (x / gcd) * y
+// $lcm = bcmul(bcdiv($x, $g), $y);
 
-echo $lcm;
+// echo $lcm;
+
